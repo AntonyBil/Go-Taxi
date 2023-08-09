e7663b8332d7c502b2e0a16f584c97f255b90c78
AntonyBil <beletskiy.ant@gmail.com>
2023-08-09 22:33:48 +0300

    Initial commit

---
+++ Go Taxi/Go_TaxiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Go_TaxiApp.swift
+//  Go Taxi
+//
+//  Created by apple on 09.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Go_TaxiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
