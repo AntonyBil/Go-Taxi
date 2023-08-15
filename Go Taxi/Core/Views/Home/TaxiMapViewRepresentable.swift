4fc7cfdfc1b847b4c4c23fdb5880391dda91752c
AntonyBil <beletskiy.ant@gmail.com>
2023-08-15 22:58:26 +0300

    Generating route to destination

--- Go Taxi/Core/Views/Home/TaxiMapViewRepresentable.swift
+++ Go Taxi/Core/Views/Home/TaxiMapViewRepresentable.swift
@@ -27,6 +27,7 @@ struct TaxiMapViewRepresentable: UIViewRepresentable {
     func updateUIView(_ uiView: UIViewType, context: Context) {
         if let coordinate = locationViewModel.selectedLocationCoordinate {
             context.coordinator.addAndSelectAnnotation(withCoordinate: coordinate)
+            context.coordinator.configurePolyline(withDestinationCoordinate: coordinate)
         }
     }
     
@@ -42,6 +43,7 @@ extension TaxiMapViewRepresentable {
         
         //MARK: - Properties
         let perent: TaxiMapViewRepresentable
+        var userLocationCoordinate: CLLocationCoordinate2D?
         
         //MARK: - Lifecycle
         init(perent: TaxiMapViewRepresentable) {
@@ -51,12 +53,20 @@ extension TaxiMapViewRepresentable {
         
         //MARK: - MKMapViewDelegate
         func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+            self.userLocationCoordinate = userLocation.coordinate
             let region = MKCoordinateRegion(
                 center: CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude),
                 span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
             )
             
             perent.mapView.setRegion(region, animated: true)
+        }
+        
+        func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+            let polyline = MKPolylineRenderer(overlay: overlay)
+            polyline.strokeColor = .systemBlue
+            polyline.lineWidth = 6
+            return polyline
         }
         
         //MARK: - Helpers
@@ -69,6 +79,35 @@ extension TaxiMapViewRepresentable {
             perent.mapView.selectAnnotation(anno, animated: true)
             
             perent.mapView.showAnnotations(perent.mapView.annotations, animated: true)
+        }
+        
+        func configurePolyline(withDestinationCoordinate coordinate: CLLocationCoordinate2D) {
+            guard let userLocationCoordinate = self.userLocationCoordinate else { return }
+            getDestinationRoute(from: userLocationCoordinate,
+                                to: coordinate) { route in
+                self.perent.mapView.addOverlay(route.polyline)
+            }
+        }
+        
+        func getDestinationRoute(from userLocation: CLLocationCoordinate2D,
+                                 to destination: CLLocationCoordinate2D,
+                                 completion: @escaping(MKRoute)-> Void) {
+            let userPlacemark = MKPlacemark(coordinate: userLocation)
+            let destPlacemark = MKPlacemark(coordinate: destination)
+            let request = MKDirections.Request()
+            request.source = MKMapItem(placemark: userPlacemark)
+            request.destination = MKMapItem(placemark: destPlacemark)
+            let directions = MKDirections(request: request)
+            
+            directions.calculate { response, error in
+                if let error = error {
+                    print("DEBUG: Faild to get directions with error \(error.localizedDescription)")
+                    return
+                }
+                
+                guard let route = response?.routes.first else { return }
+                completion(route)
+            }
         }
     }
 }
